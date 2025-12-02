934861531cc1483ba80c282838640d5348b49644
nickpapgd <nickpapgd@outlook.com>
2025-12-02 23:16:31 +0200

    fix: main fixes print

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hell");
+        System.out.println("Hello Coding");
+        System.out.println("Hello Coding Festival!!!!!");
     }
 }
